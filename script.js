94f01d059279f8b6d8db8983124af162e2cefe11
Jagmohan-cmd <141887538+Jagmohan-cmd@users.noreply.github.com>
2023-11-17 08:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let first = prompt("Enter first number");
+let second = prompt("Enter second number");
+let output = alert("The final result is: " + (first * 2 + parseInt(second)));
+
